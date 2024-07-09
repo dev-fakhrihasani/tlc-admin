50a52a10ff006b63238fea97075795c8faddc3cc
dev-afhasani <dev.afhasani@gmail.com>
2024-07-09 13:32:50 +0700

    Update PendaftarModel

--- app/Models/Pendaftar.php
+++ app/Models/Pendaftar.php
@@ -9,7 +9,5 @@ class Pendaftar extends Model
 {
     use HasFactory;
 
-    protected $fiillable = [
-        'nama', 'email', 'tempat_lahir', 'tanggal_lahir', 'no_hp', 'jenjang_pendidikan', 'asal_sekolah', 'kelas', 'peminatan', 'alasan_join', 'ss1', 'ss2', 'ss3', 'ss4'
-    ];
+    protected $guarded = ['id'];
 }

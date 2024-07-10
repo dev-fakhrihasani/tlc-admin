a2a0db50a0dd05f2f44f833e395f01177fa3cd67
dev-afhasani <dev.afhasani@gmail.com>
2024-07-10 15:33:34 +0700

    Add route pendaftar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PendaftarController;
 
 Route::get('/', function () {
     return view('layouts/main');
 });
+
+Route::resource('pendaftar', PendaftarController::class);

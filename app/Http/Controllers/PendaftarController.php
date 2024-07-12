7c67a9beb7224d4ae359195cd942cf0c567f3264
dev-afhasani <dev.afhasani@gmail.com>
2024-07-12 14:41:13 +0700

    Move validation to form request

--- app/Http/Controllers/PendaftarController.php
+++ app/Http/Controllers/PendaftarController.php
@@ -40,24 +40,7 @@ class PendaftarController extends Controller
      */
     public function store(StorePendaftarRequest $request)
     {
-        $request->validate([
-            'nama' => 'required',
-            'email' => 'required|email',
-            'tempat_lahir' => 'required',
-            'tanggal_lahir' => 'required|date',
-            'no_hp' => 'required',
-            'jenjang_pendidikan' => 'required|in:SMP/MTs,SMA/SMK/MA',
-            'asal_sekolah' => 'required',
-            'kelas' => 'required',
-            'peminatan' => 'required|in:SNBT,Kedinasan',
-            'alasan_join' => 'required',
-            'ss1' => 'image|file|max:2048',
-            'ss2' => 'image|file|max:2048',
-            'ss3' => 'image|file|max:2048',
-            'ss4' => 'image|file|max:2048',
-        ]);
-
-        $data = $request->all();
+        $data = $request->validated();
 
         foreach (['ss1', 'ss2', 'ss3', 'ss4'] as $ss) {
             if ($request->hasFile($ss)) {

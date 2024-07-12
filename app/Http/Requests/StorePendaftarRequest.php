d526acbc578679f048af6e7a406f1a2e2e0c2897
dev-afhasani <dev.afhasani@gmail.com>
2024-07-12 14:41:27 +0700

    Create rules of validation

--- app/Http/Requests/StorePendaftarRequest.php
+++ app/Http/Requests/StorePendaftarRequest.php
@@ -22,7 +22,20 @@ class StorePendaftarRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nama' => 'required',
+            'email' => 'required|email',
+            'tempat_lahir' => 'required',
+            'tanggal_lahir' => 'required|date',
+            'no_hp' => 'required',
+            'jenjang_pendidikan' => 'required|in:SMP/MTs,SMA/SMK/MA',
+            'asal_sekolah' => 'required',
+            'kelas' => 'required',
+            'peminatan' => 'required|in:SNBT,Kedinasan',
+            'alasan_join' => 'required',
+            'ss1' => 'image|file|max:2048',
+            'ss2' => 'image|file|max:2048',
+            'ss3' => 'image|file|max:2048',
+            'ss4' => 'image|file|max:2048',
         ];
     }
 }

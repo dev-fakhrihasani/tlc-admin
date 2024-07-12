8e3ef06d50330bea6d952286919ac6a63dc80329
dev-afhasani <dev.afhasani@gmail.com>
2024-07-12 14:50:07 +0700

    Update main view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,26 +7,8 @@
     <title>TLC Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&display=swap');
-
-        body {
-            background-color: floralwhite;
-        }
-
-        nav {
-            font-family: "Inter Tight", sans-serif;
-            font-weight: 500;
-            font-size: 14px;
-        }
-
-        .content {
-            font-family: "Inter Tight", sans-serif;
-            font-weight: 400;
-            letter-spacing: 0.3px;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
@@ -41,10 +23,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav nav-underline">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Dashboard</a>
+                        <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pendaftar</a>
+                        <a class="nav-link {{ Request::is('pendaftar*') ? 'active' : '' }}" href="/pendaftar">Pendaftar</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Blog</a>
@@ -55,13 +37,7 @@
                 </ul>
                 <ul class="ms-auto navbar-nav nav-underline">
                     <li class="nav-item">
-                        <!-- <a class="nav-link" href="#"> -->
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                            </svg> -->
-                        <!-- </a> -->
-                        <button class="btn btn-outline-danger">Logout</button>
+                        <button class="btn btn-outline-light">Logout</button>
                     </li>
                 </ul>
             </div>
@@ -71,6 +47,7 @@
         @yield('content')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.2/dist/sweetalert2.all.min.js"></script>
 </body>
 
 </html>
